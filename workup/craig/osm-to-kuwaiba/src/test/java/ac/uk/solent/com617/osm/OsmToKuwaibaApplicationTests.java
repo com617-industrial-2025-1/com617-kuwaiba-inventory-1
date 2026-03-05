03f61a51a8dbe3be831e2191589319c9564e1316
gallenc <cg02r@zepler.net>
2026-03-05 14:40:32 +0000

    craigs example spring boot

---
+++ workup/craig/osm-to-kuwaiba/src/test/java/ac/uk/solent/com617/osm/OsmToKuwaibaApplicationTests.java
@@ -0,0 +1,13 @@
+package ac.uk.solent.com617.osm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsmToKuwaibaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
