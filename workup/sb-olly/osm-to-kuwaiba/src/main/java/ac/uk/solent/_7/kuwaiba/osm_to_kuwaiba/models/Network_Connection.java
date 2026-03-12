656f5f9ade89dcbda5d93e10b092f68f807ac371
45inertia <5myero37@solent.ac.uk>
2026-03-12 14:13:06 +0000

    Added the Network Point Model.

--- workup/sb-olly/osm-to-kuwaiba/src/main/java/ac/uk/solent/_7/kuwaiba/osm_to_kuwaiba/models/Network_Connection.java
+++ workup/sb-olly/osm-to-kuwaiba/src/main/java/ac/uk/solent/_7/kuwaiba/osm_to_kuwaiba/models/Network_Connection.java
@@ -4,9 +4,11 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import org.locationtech.jts.geom.LineString;
 
 @Entity
+@Table(name = "network_connection")
 public class Network_Connection {
     @Id
     private long id;
@@ -15,7 +17,6 @@ public class Network_Connection {
     private long end_id;
     private String link_type;
 
-    @JsonIgnore
     @Column(columnDefinition = "geometry(LineString, 4326)")
     private LineString geom;
 
@@ -24,6 +25,8 @@ public class Network_Connection {
     public String getExternal_id() { return external_id; }
     public long getStart_id() { return start_id; }
     public long getEnd_id() { return end_id; }
+
+    @JsonIgnore
     public LineString getGeom() { return geom; }
 
     public void setId(long id) { this.id = id; }
--- workup/sb-olly/osm-to-kuwaiba/src/main/java/ac/uk/solent/_7/kuwaiba/osm_to_kuwaiba/models/Network_Point.java
+++ workup/sb-olly/osm-to-kuwaiba/src/main/java/ac/uk/solent/_7/kuwaiba/osm_to_kuwaiba/models/Network_Point.java
@@ -1,4 +1,31 @@
 package ac.uk.solent._7.kuwaiba.osm_to_kuwaiba.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import org.locationtech.jts.geom.Point;
+
+@Entity
+@Table(name = "network_point")
 public class Network_Point {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String external_id;
+    private String type;
+
+    @Column(name = "point_type")
+    private Point geom;
+
+    // getters and setters
+    public long getId() { return id; }
+    public String getExternal_id() { return external_id; }
+    public String getType() { return type; }
+
+    @JsonIgnore
+    public Point getGeom() { return geom; }
+
+    public void setId(long id) { this.id = id; }
+    public void setExternal_id(String ex_id) { this.external_id = ex_id; }
+    public void setType(String type) { this.type = type; }
+    public void setGeom(Point geom) { this.geom = geom; }
 }
