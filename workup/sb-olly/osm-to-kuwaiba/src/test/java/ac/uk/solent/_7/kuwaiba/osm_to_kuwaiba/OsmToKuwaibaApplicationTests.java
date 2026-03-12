21c34fcb7999ff0ae077680422c6a27c2094ba1b
45inertia <5myero37@solent.ac.uk>
2026-03-12 13:11:33 +0000

    Initial commit for alternate Spring Boot Project

---
+++ workup/sb-olly/osm-to-kuwaiba/src/test/java/ac/uk/solent/_7/kuwaiba/osm_to_kuwaiba/OsmToKuwaibaApplicationTests.java
@@ -0,0 +1,13 @@
+package ac.uk.solent._7.kuwaiba.osm_to_kuwaiba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsmToKuwaibaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
