3b5945751dc766d18da5d11a4441d60f075f6f78
45inertia <5myero37@solent.ac.uk>
2026-03-19 08:57:19 +0000

    small update to routing service and copied docker-compose.yml over from data processing.

--- workup/sb-olly/osm-to-kuwaiba/README.md
+++ workup/sb-olly/osm-to-kuwaiba/README.md
@@ -2,3 +2,5 @@
 
 ## Improvements
 - Place all points (except building drop points) on a road.
+- Connections from exchanges to poles should follow the roads.
+- 
--- workup/sb-olly/osm-to-kuwaiba/src/main/java/ac/uk/solent/_7/kuwaiba/osm_to_kuwaiba/services/RoutingService.java
+++ workup/sb-olly/osm-to-kuwaiba/src/main/java/ac/uk/solent/_7/kuwaiba/osm_to_kuwaiba/services/RoutingService.java
@@ -1,8 +1,6 @@
 package ac.uk.solent._7.kuwaiba.osm_to_kuwaiba.services;
 
-import ac.uk.solent._7.kuwaiba.osm_to_kuwaiba.repository.CleanedBuildingRepository;
 import ac.uk.solent._7.kuwaiba.osm_to_kuwaiba.repository.NetworkConnectionRepository;
-import ac.uk.solent._7.kuwaiba.osm_to_kuwaiba.repository.NetworkPointRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,15 +11,9 @@ public class RoutingService {
     @Autowired
     private NetworkConnectionRepository connectionRepository;
 
-    @Autowired
-    private NetworkPointRepository pointRepository;
-
-    @Autowired
-    private CleanedBuildingRepository buildingRepository;
-
     @Transactional
     public String runFullPrediction() {
-        connectionRepository.deleteAll();
+        connectionRepository.deleteAll(); // clears all previous connections
 
         exchangeToAggregatorPrediction();
         aggregatorToCabinetPrediction();
@@ -32,19 +24,19 @@ public class RoutingService {
     }
 
     public void exchangeToAggregatorPrediction() {
-
+        connectionRepository.insertDropConnections();
     }
 
     public void aggregatorToCabinetPrediction() {
-
+        connectionRepository.insertFeederConnections();
     }
 
     public void cabinetToPolePrediction() {
-
+        connectionRepository.insertDistributionConnections();
     }
 
     public void poleToBuildingPrediction() {
-
+        connectionRepository.insertTrunkConnections();
     }
 
 }
