30520e344194e363f6d5831603975d2de44d84aa
45inertia <5myero37@solent.ac.uk>
2026-03-21 11:45:59 +0000

    test(repo and services): added basics
    Just added some of the Spring Boot stuff that I will need to write the
    tests.

--- workup/sb-olly/osm-to-kuwaiba/src/test/java/ac/uk/solent/_7/kuwaiba/osm_to_kuwaiba/repository/NetworkConnectionTests.java
+++ workup/sb-olly/osm-to-kuwaiba/src/test/java/ac/uk/solent/_7/kuwaiba/osm_to_kuwaiba/repository/NetworkConnectionTests.java
@@ -1,5 +1,19 @@
 package ac.uk.solent._7.kuwaiba.osm_to_kuwaiba.repository;
 
-public class NetworkConnectionTests {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.transaction.annotation.Transactional;
 
+@SpringBootTest
+@Transactional
+public class NetworkConnectionTests {
+	
+	@Autowired
+	private NetworkConnectionRepository connectionRepository;
+	
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
+	
 }
--- workup/sb-olly/osm-to-kuwaiba/src/test/java/ac/uk/solent/_7/kuwaiba/osm_to_kuwaiba/services/ClusteringServiceTests.java
+++ workup/sb-olly/osm-to-kuwaiba/src/test/java/ac/uk/solent/_7/kuwaiba/osm_to_kuwaiba/services/ClusteringServiceTests.java
@@ -1,5 +1,11 @@
 package ac.uk.solent._7.kuwaiba.osm_to_kuwaiba.services;
 
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
 public class ClusteringServiceTests {
+	
 
 }
