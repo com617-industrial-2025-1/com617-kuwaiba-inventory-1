4a7209676a9de40652a5c74b1087cb11a265967c
gallenc <cg02r@zepler.net>
2026-06-18 13:37:14 +0100

    template allows creation of new state and city - not yet continent

--- osm-to-kuwaiba/osm-to-kuwaiba-spring-boot/src/main/java/ac/uk/solent/com617/kuwaiba/osm_to_kuwaiba/services/ClusteringService.java
+++ osm-to-kuwaiba/osm-to-kuwaiba-spring-boot/src/main/java/ac/uk/solent/com617/kuwaiba/osm_to_kuwaiba/services/ClusteringService.java
@@ -1,10 +1,10 @@
 package ac.uk.solent.com617.kuwaiba.osm_to_kuwaiba.services;
 
-import org.entimoss.kuwaiba.provisioning.model.ProjectConstants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import ac.uk.solent.com617.kuwaiba.osm_to_kuwaiba.config.ProjectConstants;
 import ac.uk.solent.com617.kuwaiba.osm_to_kuwaiba.repository.BuildingDropPointRepository;
 import ac.uk.solent.com617.kuwaiba.osm_to_kuwaiba.repository.CleanedBuildingRepository;
 import ac.uk.solent.com617.kuwaiba.osm_to_kuwaiba.repository.CleanedRoadRepository;
@@ -13,8 +13,6 @@ import ac.uk.solent.com617.kuwaiba.osm_to_kuwaiba.repository.NetworkPointReposit
 
 @Service
 public class ClusteringService {
-   
-   private String networkRegion = ProjectConstants.PARENT_LOCATION_VALUE;
 
     @Autowired
     private BuildingDropPointRepository dropPointRepository;
@@ -30,6 +28,9 @@ public class ClusteringService {
     
     @Autowired
     private NetworkConnectionRepository networkRepository;
+    
+    @Autowired
+    private ProjectConstants projectConstantValues;
 
     @Transactional // all methods are treated as an all or nothing incase things go wrong.
     public String runFullPrediction() {
@@ -46,21 +47,25 @@ public class ClusteringService {
     }
 
     public void predictPoles() {
+       String networkRegion = projectConstantValues.getParentLocationName();
         dropPointRepository.insertPoleClusters(networkRegion);
         dropPointRepository.updateBuildingParents();
     }
 
     public void predictCabinets() {
+       String networkRegion = projectConstantValues.getParentLocationName();
         pointRepository.insertCabinetClusters(networkRegion);
         pointRepository.updatePoleParents();
     }
 
     public void predictAggregators() {
+       String networkRegion = projectConstantValues.getParentLocationName();
         pointRepository.insertAggregatorClusters(networkRegion);
         pointRepository.updateCabinetParents();
     }
 
     public void predictExchanges() {
+       String networkRegion = projectConstantValues.getParentLocationName();
         pointRepository.insertExchangeClusters(networkRegion);
         pointRepository.updateAggregatorParents();
     }
